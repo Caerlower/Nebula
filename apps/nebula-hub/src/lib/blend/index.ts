938fc8f1355db499d23204f1691815e261e14b2d
Manav Goyal <manucool123.rich@gmail.com>
2026-07-25 22:44:59 +0530

    feat(hub): re-export network-aware Blend helpers

--- apps/nebula-hub/src/lib/blend/index.ts
+++ apps/nebula-hub/src/lib/blend/index.ts
@@ -1,15 +1,38 @@
 export {
+  BLEND_MAINNET_POOL,
+  BLEND_MAINNET_POOLS,
+  BLEND_MAINNET_USDC_ASSET,
+  BLEND_MAINNET_XLM_ASSET,
   BLEND_TESTNET_POOL,
   BLEND_TESTNET_POOLS,
+  BLEND_TESTNET_USDC_ASSET,
   BLEND_TESTNET_XLM_ASSET,
+  assertBlendAssetSupported,
+  blendAssetId,
+  blendXlmAsset,
   floorXlm,
   getBlendPoolsForNetwork,
   resolvePool,
   roundXlm,
+  supportedBlendAssets,
   xlmFeeBuffer,
 } from "./config";
-export { getTreasuryBalances, getLiquidXlm, getBlendDepositedXlm } from "./balances";
+export type { BlendAsset, BlendPoolConfig, HubNetwork } from "./config";
+export {
+  getTreasuryBalances,
+  getLiquidXlm,
+  getLiquidUsdc,
+  getBlendDeposited,
+  getBlendDepositedXlm,
+  listBlendPositions,
+} from "./balances";
+export type { BlendPositionRow, TreasuryBalances } from "./balances";
 export { fetchBlendSupplyRates } from "./rates";
-export { blendDepositXlm, blendWithdrawXlm, blendWithdrawAndPay } from "./transactions";
+export {
+  blendDeposit,
+  blendDepositXlm,
+  blendWithdraw,
+  blendWithdrawXlm,
+  blendWithdrawAndPay,
+} from "./transactions";
 export type { BlendSubmitResult, BlendPayBundleResult } from "./transactions";
-export type { TreasuryBalances } from "./balances";
